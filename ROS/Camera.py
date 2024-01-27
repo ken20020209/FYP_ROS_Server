b81ae5c65317f98eb00923b7bcd8115861db4d25
ken20020209 <ken20020209@gmail.com>
2024-01-27 19:02:35 +0800

    complete basic get image and pub image

--- ROS/Camera.py
+++ ROS/Camera.py
@@ -11,9 +11,36 @@ import numpy
 #ros2 lib
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import Image, CompressedImage
+
 
 class Camera(Node):
+
+    #dummy processer
+    class DummyProcesser:
+        def __init__(self):
+            pass
+        def process(self,img):
+            return img
+    #save procesor on setting
+    setting:dict = {}
+    processor:any = DummyProcesser()
+
     def __init__(self,name='Camera'):
         super().__init__(name)
+        self.subCamera = self.create_subscription(Image, 'camera/raw', self.subCammera, 10)
+        self.pubCamera = self.create_publisher(Image, 'camera/effected', 10)
+        self.bridge = CvBridge()
+        self.img = None
+        self.img_effected = None
+    
+    def subCammera(self,msg):
+        # get image from dog camera and process it
+        self.img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        self.img_effected = self.processor.process(self.img)
+        msg_effected = self.bridge.cv2_to_imgmsg(self.img_effected, "bgr8")
+        self.pubCamera.publish(msg_effected)
+        
+    def process(self,):
         raise NotImplementedError
     
