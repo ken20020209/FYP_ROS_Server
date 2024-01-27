502b4313d8507449c5b844fcbe8f02f1da8b0e1c
ken20020209 <ken20020209@gmail.com>
2024-01-27 22:26:14 +0800

    change camera use copreseedImage

--- ROS/Camera.py
+++ ROS/Camera.py
@@ -28,19 +28,31 @@ class Camera(Node):
 
     def __init__(self,name='Camera'):
         super().__init__(name)
-        self.subCamera = self.create_subscription(Image, 'camera/raw', self.subCammera, 10)
-        self.pubCamera = self.create_publisher(Image, 'camera/processed', 10)
+        # self.subCamera = self.create_subscription(Image, 'camera/raw', self.subCammera, 10)
+        self.subCamera = self.create_subscription(CompressedImage, 'camera/raw', self.subCammera, 10)
+        self.pubCamera = self.create_publisher(CompressedImage, 'camera/processed', 10)
         self.bridge = CvBridge()
         self.img = None
         self.img_effected = None
+
+        print("Camera init success")
     
     def subCammera(self,msg):
         # get image from dog camera and process it
-        self.img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        self.img = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
         self.img_effected = self.processor.process(self.img)
-        msg_effected = self.bridge.cv2_to_imgmsg(self.img_effected, "bgr8")
+        msg_effected = self.bridge.cv2_to_compressed_imgmsg(self.img_effected)
         self.pubCamera.publish(msg_effected)
         
     def process(self,):
         raise NotImplementedError
+def main():
+    rclpy.init()
+    camera = Camera()
+    rclpy.spin(camera)
+    camera.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
     
