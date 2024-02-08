5a078945edef2fc167fc96b144a75040e4336096
ken20020209 <ken20020209@gmail.com>
2024-02-08 17:39:31 +0800

    rename launch file

--- ROS_Server_WS/src/basic/basic/RobotDogConnector.py
+++ ROS_Server_WS/src/basic/basic/RobotDogConnector.py
@@ -19,7 +19,7 @@ from service.msg import DogStatus
 
 def startController(port,rosDomainId):
     os.environ['ROS_DOMAIN_ID'] = str(rosDomainId)
-    os.system(f"ros2 launch basic RobotDogController.launch.py port:={port}")
+    os.system(f"ros2 launch basic Controller.launch.py port:={port}")
 
 class RobotDogConnector(Node):
     #save the dog that registered
@@ -72,7 +72,7 @@ class RobotDogConnector(Node):
         #start the controller and rosbrige with port and rosDomainId with subprocess
         sp_env=os.environ.copy()
         sp_env['ROS_DOMAIN_ID'] = str(rosDomainId)
-        sp = subprocess.Popen(["ros2","launch","basic","RobotDogController.launch.py",f"port:={port}"],env=sp_env)
+        sp = subprocess.Popen(["ros2","launch","basic","Controller.launch.py",f"port:={port}"],env=sp_env)
         self.dogList[request.dog_id]["process"] = sp
         #---------------------------------------------------
 
