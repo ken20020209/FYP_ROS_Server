34cefbc5de3e67d4581ef413cbbc76faf7997e88
ken20020209 <ken20020209@gmail.com>
2024-02-17 20:29:53 +0800

    Add camera setting subscription

--- ROS_Server_WS/src/basic/basic/Camera.py
+++ ROS_Server_WS/src/basic/basic/Camera.py
@@ -12,6 +12,7 @@ import numpy
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image, CompressedImage
+from std_msgs.msg import Int32
 
 
 class Camera(Node):
@@ -31,12 +32,18 @@ class Camera(Node):
         # self.subCamera = self.create_subscription(Image, 'camera/raw', self.subCammera, 10)
         self.subCamera = self.create_subscription(CompressedImage, 'camera/raw', self.subCammera, 10)
         self.pubCamera = self.create_publisher(CompressedImage, 'camera/processed', 10)
+        self.subCameraSetting = self.create_subscription(Int32, 'camera/setting', self.subSetting, 10)
         self.bridge = CvBridge()
         self.img = None
         self.img_effected = None
 
         print("Camera init success")
-    
+    def subSetting(self,msg):
+        # get setting from setting camera and process it
+        # tODO: save setting to setting wait for ai effect
+        pass
+
+
     def subCammera(self,msg):
         # get image from dog camera and process it
         self.img = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
