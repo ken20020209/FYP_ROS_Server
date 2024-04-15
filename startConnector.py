7a205bff2c2dc5435d290fdac8216b5b98c49095
ken20020209 <94442724+ken20020209@users.noreply.github.com>
2024-04-15 22:44:37 +0800

    Update IP management functions and API endpoints in Connector.py

--- startConnector.py
+++ startConnector.py
@@ -2,11 +2,46 @@ import os
 from time import sleep
 from threading import Thread
 from multiprocessing import Process,Manager,Value
+import requests
 
 import subprocess
 import socket
 import json
 
+
+def gethostIP():
+    try:
+        url = os.getenv('ROS_PROXY_IP_URL','http://localhost:9088/api/ros/ip')
+        payload = {}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        if(response.status_code!=200):
+            return ''
+        data=response.json()
+        ip=data.get('ip')
+        if(ip is None):
+            return ''
+        return ip
+    except Exception as e:
+        print(str(e))
+        return ''
+def putHostIP(ip) -> bool:
+    try:
+        import requests
+        url = os.getenv('ROS_PROXY_URL','http://localhost:9089') +'/api/ros/ip'
+        payload = json.dumps({
+            "ip":ip
+        })
+        headers = {
+        'Content-Type': 'application/json'
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        if(response.status_code!=200):
+            return False
+        return True
+    except Exception as e:
+        print(str(e))
+        return False
 
 # if(os.path.exists(f"{os.path.dirname(os.path.abspath(__file__))}/install/setup.bash")):
 #     with open(f"{os.path.dirname(os.path.abspath(__file__))}/env.json") as f:
@@ -15,8 +50,9 @@ import json
 #             k=k.upper()
 #             os.environ[k]=v
 
-discovery_server =os.getenv("discover_server" ,"discoveryserver.ddns.net")
+discovery_server =os.getenv("discover_server" ,"discovery.ken20020209.com")
 
+count=0
 
 def get_ip_address(domain_name):
     try:
@@ -25,6 +61,28 @@ def get_ip_address(domain_name):
         return "127.0.0.1"
     
 discovery_server_ip=get_ip_address(discovery_server)
+
+while count<10 and discovery_server_ip!="127.0.0.1":
+    print(f"get host ip from {discovery_server_ip}")
+    ip=gethostIP()
+    if(ip==""):
+        sleep(1)
+        continue
+    discovery_server_ip=ip
+    count+=1
+    break
+if(count>=10):
+    exit(1)
+count=0
+while count<10 and discovery_server_ip!="127.0.0.1":
+    print(f"put host ip {discovery_server_ip}")
+    if(putHostIP(discovery_server_ip)):
+        sleep(1)
+        break
+    count+=1
+if(count>=10):
+    exit(1)
+
 print(f"{discovery_server}:{get_ip_address(discovery_server)}")
 cmd=f"/bin/bash -c"
 cmd+=f" '"
